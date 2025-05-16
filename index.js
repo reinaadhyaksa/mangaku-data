8c9c20974d9e73a6ccacde450849de8845e2d674
Reina Elsy <violetevargarde@gmail.com>
2025-05-16 10:32:37 +0700

    data

--- index.js
+++
@@ -1,7 +0,0 @@
-import { BorutoTwoBlueFortex } from "./data/BorutoTwoBlueFortex";
-import { WindBreaker } from "./data/WindBreaker";
-import { JujutsuKaisen } from "./data/JujutsuKaisen";
-
-export const  dataKomik = [
-  BorutoTwoBlueFortex, WindBreaker, JujutsuKaisen,
-];
